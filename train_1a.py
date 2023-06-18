ce8d93f9e274c83f5fb741938ed4e26f285322bc
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-06-18 14:07:14 -0300

    Copy main from tp3

--- train_1a.py
+++ train_1a.py
@@ -1,5 +1,24 @@
+import utils
+from src.multi_layer_perceptron import MultiLayerPerceptron
+
 def main():
-    print("Hello World!")
+    settings = utils.get_settings()
+    cut_condition = utils.get_cut_condition(settings)
+    activation_method = utils.get_activation_function(settings)
+    optimization_method = utils.get_optimization_method(settings)
+    epochs = utils.get_epochs(settings)
+
+    mlp = MultiLayerPerceptron([2, 2, 1],
+                               epochs,
+                               cut_condition,
+                               activation_method,
+                               optimization_method)
+    print(f"Training finished in {len(mlp.train_batch(X, EXPECTED))} epochs.")
+
+    ans = perceptron.predict(X)
+    for test in range(X.shape[0]):
+        print(f"{X[test][0]} & {X[test][1]} = {ans[test]}")
+
 
 if __name__ == "__main__":
     main()
